245c659179d171f52c80648088cae7d091b354be
Lzh10 <lzh1029384756@outlook.com>
2023-05-06 20:56:01 +0800

    Add files via upload

--- 2（仅整合+查错）格式工具.py
+++ 2（仅整合+查错）格式工具.py
@@ -1,9 +1,29 @@
-# 导入os模块
 import os
+import codecs
+import webbrowser
+
+try:
+    import chardet
+except ImportError:
+    print("chardet模块没有安装，你可以使用pip install chardet或者conda install chardet命令进行安装。")
+
+# 将源文件转换成UTF-8格式
+def convert_file_encoding_to_utf8(src_path):
+    # 检测源文件编码
+    with open(src_path, 'rb') as f:
+        rawdata = f.read()
+    encoding = chardet.detect(rawdata)['encoding']
+    
+    # 如果编码不是UTF-8，进行转换
+    if encoding and encoding.lower() != 'utf-8':
+        with codecs.open(src_path, 'r', encoding=encoding) as f:
+            text = f.read()
+        with codecs.open(src_path, 'w', encoding='utf-8') as f:
+            f.write(text)
 
 # 复制源文件到目标文件中
 def copy_lines(src_path, dst_file):
-    with open(src_path, 'r') as f:
+    with codecs.open(src_path, 'r', encoding='utf-8') as f:
         for line in f:
             # 将原始文本中的每一行添加到目标文件中，并去掉末尾的换行符
             dst_file.write(line.strip() + '\n')
@@ -13,24 +33,25 @@ def check_gtavc_file(file_path, crash_dump_file):
     # 用一个集合来存储重复的文本行和上一行
     dup_lines = set()
     last_line = None
-    with open(file_path, 'r') as f:
+    with codecs.open(file_path, 'r', encoding='utf-8') as f:
         for i, line in enumerate(f):
             # 跳过空行和注释
-            if not line.strip() or line.startswith('//'):
+            if not line.strip():
                 continue
-            
+            if line.startswith('//'):
+                continue
+            # 以下修改
+            if ';' in line:
+                continue
+            if line.strip().startswith('//'):
+                continue
+            # 修改结束
             # 检查等号前面的空格
             if '=' in line:
                 equal_idx = line.index('=')
                 if line[equal_idx-1] == ' ':
                     # 如果等号前有空格，输出错误信息
                     crash_dump_file.write(f'{file_path}:{i+1}: {line.strip()} (=号出现了空格啊！！！！！！)\n')
-            
-            # 检查重复行
-            if last_line is not None and last_line.split('=')[0] == line.split('=')[0]:
-                dup_lines.add(last_line)
-                dup_lines.add(line)
-            last_line = line
             
             # 检查同一行上的多个等号
             if line.count('=') > 1:
@@ -43,10 +64,25 @@ def check_gtavc_file(file_path, crash_dump_file):
             # 检查等号前面是否有内容
             if not line.split('=')[0].strip():
                 crash_dump_file.write(f'{file_path}:{i+1}: {line.strip()} (符号=前面缺少内容啊！！！！！！)\n')
-    
-    # 输出重复行
-    for dup_line in sorted(list(dup_lines)):
-        crash_dump_file.write(f'{file_path}:n/a: {dup_line.strip()} (有几个相同前缀了啊！！！！！！)\n')
+            
+            # 添加判断，如果行中没有[或]或=三个之一，就将本行输出至CrashDump.txt
+            if '[' not in line and ']' not in line and '=' not in line:
+                crash_dump_file.write(f'{file_path}:{i+1}: {line.strip()} (行中没有[或]或=三个之一)\n')
+
+            # 检查符号=前的内容是否与之前的某行完全匹配
+            if last_line is not None and last_line.strip().startswith(line.split('=')[0].strip()):
+                crash_dump_file.write(f'{file_path}:{i+1}: {line.strip()} (符号=前的内容与之前某一行完全匹配啊！！！！！！)\n')
+            last_line = line
+            
+            # 检查重复行（完全相同）
+            if line == last_line:
+                dup_lines.add(last_line)
+                dup_lines.add(line)
+            # 检查重复行（符号=前的内容相同）
+            elif last_line is not None and last_line.split('=')[0] == line.split('=')[0]:
+                dup_lines.add(last_line)
+                dup_lines.add(line)
+            last_line = line
 
 # 主函数
 def main():
@@ -54,25 +90,26 @@ def main():
     gtavc_file = 'gtavc.txt'
     crash_dump_file = 'CrashDump.txt'
     # 获取当前文件夹中所有的文本文件
-    file_list = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.txt')]
+    file_list = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.txt') and f != gtavc_file and f != crash_dump_file and f != 'CHARACTERS.txt']
+    # 将所有文本文件的编码格式转换为utf-8
+    for file_path in file_list:
+        convert_file_encoding_to_utf8(file_path)
+    
     # 将所有文本文件的内容复制到gtavc文本文件中
-    with open(gtavc_file, 'w') as dst_file:
+    with open(gtavc_file, 'w', encoding='utf-8') as dst_file:
         for file_path in file_list:
-            # 忽略gtavc.txt和CrashDump.txt文件
-            if file_path == gtavc_file or file_path == crash_dump_file:
-                continue
             copy_lines(file_path, dst_file)
 
     # 检查所有文本文件中的错误并输出到crash dump文件中
-    with open(crash_dump_file, 'w') as dump_file:
+    with open(crash_dump_file, 'w', encoding='utf-8') as dump_file:
         for file_path in file_list:
-            # 忽略gtavc.txt和CrashDump.txt文件
-            if file_path == gtavc_file or file_path == crash_dump_file:
-                continue
             check_gtavc_file(file_path, dump_file)
             
         # 在CrashDump.txt文件的末尾添加结束语
         dump_file.write('\n此工具Lzh10_慕黑倾情制作，感谢您使用英文文本整合工具。\nGithub: https://github.com/lzh102938\n哔哩哔哩: https://space.bilibili.com/1657432204\n个人Blog: https://lzh10.netlify.app\n个人小游戏: https://lzhgames.netlify.app\nQQ: 235810290\n邮箱: lzh1029384756@outlook.com※235810290@qq.com\n回首向来萧瑟处，归去，也无风雨也无晴。')
+    
+    # 打开生成的CrashDump.txt文件
+    webbrowser.open(crash_dump_file)
 
 if __name__ == '__main__':
     main()
