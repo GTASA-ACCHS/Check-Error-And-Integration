ddfe958ea8ebc7f8b69f6baa9ae6502917858cb8
Lzh10 <lzh1029384756@outlook.com>
2023-05-20 09:45:37 +0800

    Add files via upload

--- 1（仅转换格式）格式工具.py
+++ 1（仅转换格式）格式工具.py
@@ -9,7 +9,7 @@ for file in os.listdir("."):
 # 针对每个文件逐行处理
 for file_name in file_list:
     new_lines = []
-    with open(file_name, "r") as file:
+    with open(file_name, "r", encoding="utf-8") as file:
         for line in file:
             # 如果行不包含[或]字符，则进行复制粘贴并加入分号
             if "[" not in line and "]" not in line:
@@ -17,5 +17,5 @@ for file_name in file_list:
             # 否则保留该行不进行处理
             else:
                 new_lines.append(line)
-    with open(file_name, "w") as file:
+    with open(file_name, "w", encoding="utf-8") as file:
         file.writelines(new_lines)
